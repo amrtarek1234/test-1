8e0ca063b09de2b77de85fdb11219b1d3c9e87bf
amrtarek1234 <134492508+amrtarek1234@users.noreply.github.com>
2024-09-08 11:58:52 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,10 +3,10 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.7.0"
-      subscription_id   = ${{secrets.AZURE_SUBSCRIPTION_ID}}
-      tenant_id         = ${{secrets.AZURE_AD_TENANT_ID}}
-      client_id         = ${{secrets.AZURE_AD_CLIENT_ID}}
-      client_secret     = ${{secrets.AZURE_AD_CLIENT_SECRET}}
+      subscription_id   = "${{secrets.AZURE_SUBSCRIPTION_ID}}"
+      tenant_id         = "${{secrets.AZURE_AD_TENANT_ID}}"
+      client_id         = "${{secrets.AZURE_AD_CLIENT_ID}}"
+      client_secret     = "${{secrets.AZURE_AD_CLIENT_SECRET}}"
 
     }
   }
