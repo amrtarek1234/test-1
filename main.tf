e520877a2f3cbce0675587458fbe367d40cf7b40
amrtarek1234 <134492508+amrtarek1234@users.noreply.github.com>
2024-09-11 10:23:53 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
-resource "azurerm_resource_group" "rg-akss" {
-
+resource "azurerm_resource_group" "rg" {
 
   name     = "test-branch00"
   location =  "West Europe"
 }
 resource "azurerm_virtual_network" "webserver_vnet" {
-  name                = "vnetwork02"
+  name                = "vnetwork"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg-akss.location
-  resource_group_name = azurerm_resource_group.rg-akss.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "webserver_subnet" {
-  name                 = "internal02"
-  resource_group_name  = azurerm_resource_group.rg-akss.name
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.webserver_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
